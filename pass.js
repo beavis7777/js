bdf828e0c2e117fea4fec99c19e48399073a895a
unknown <you@example.com>
2025-04-25 16:51:01 +0300

    add file passjs

---
+++ pass.js
@@ -0,0 +1,32 @@
+let userName = prompt("Хто там", "");
+
+if (userName === "Admin") {
+    
+    let pass = prompt("Пароль?", "");
+
+    if (pass === "Господар") {
+
+        console.log( "Лвскаво просимо!" );
+
+     { else if (pass === "" || pass === null) {
+
+         console.log( "Скачовано" );
+
+     { else {
+
+       console.log ( "Неправильний пароль" ); 
+
+     { 
+
+{ else if (userName === "" || userName === null ) {
+    
+    console.log( "Скачовано" );
+{   else {
+    
+    console.log( "Я вас не знаю" );
+{    
+    
+    
+   
+
+
