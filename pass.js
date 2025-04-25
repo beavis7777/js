fdccd01950c76e7f265e3fc7ce756d7a42cfc7e0
unknown <you@example.com>
2025-04-25 17:03:32 +0300

    renovation pass.js

--- pass.js
+++ pass.js
@@ -5,25 +5,17 @@ if (userName === "Admin") {
     let pass = prompt("Пароль?", "");
 
     if (pass === "Господар") {
-
-        console.log( "Лвскаво просимо!" );
-
-     { else if (pass === "" || pass === null) {
-
-         console.log( "Скачовано" );
-
-     { else {
-
-       console.log ( "Неправильний пароль" ); 
-
-     { 
+     console.log( "Лвскаво просимо!" );
+    { else if (pass === "" || pass === null) {
+     console.log( "Скачовано" );
+    { else {
+     console.log ( "Неправильний пароль" ); 
+    { 
 
 { else if (userName === "" || userName === null ) {
-    
-    console.log( "Скачовано" );
-{   else {
-    
-    console.log( "Я вас не знаю" );
+  console.log( "Скачовано" );
+{ else {
+  console.log( "Я вас не знаю" );
 {    
     
     
