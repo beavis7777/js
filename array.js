c3cade717ebe364d60a5d2508b239e9518bc35f4
unknown <you@example.com>
2025-05-09 11:55:15 +0300

    add styles

--- array.js
+++ array.js
@@ -1,1 +1,5 @@
-console.log("array");
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert( styles.shift() );
+styles.unshift("Rap", "Reggae");
